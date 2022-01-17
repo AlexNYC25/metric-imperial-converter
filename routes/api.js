9d2039b2849cfc2b0247dbfedb69472dbb99520b
Alexis Montes <montes.alexis25@gmail.com>
2022-01-16 19:58:36 -0500

    set up route logic and response for /api/convert

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,35 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+	
+	let convertHandler = new ConvertHandler();
 
+	app.get('/api/convert', (req, res) => {
+		let conversionInput = req.query.input;
+
+		let initNum = convertHandler.getNum(conversionInput);
+		let initUnit = convertHandler.getUnit(conversionInput);
+		let returnNum = convertHandler.convert(initNum, initUnit);
+		let returnUnit = convertHandler.getReturnUnit(initUnit);
+
+		if(initNum === null && initUnit === null) {
+			return res.status(400).json('invalid number and unit');
+		} else if(initNum === null) {
+			return res.status(400).json('invalid number');
+		} else if(initUnit === null) {
+			return res.status(400).json('invalid unit');
+		} else if(returnNum === null) {
+			return res.status(400).json('invalid number');
+		} else if(returnUnit === null) {
+			return res.status(400).json('invalid unit');
+		} else {
+			return res.status(200).json({
+				initNum: initNum,
+				initUnit: initUnit,
+				returnNum: returnNum,
+				returnUnit: returnUnit
+			});
+		}
+
+	});
 };
